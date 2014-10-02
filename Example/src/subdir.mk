05763ca6643a87bdb9e7a148a68c62277079bbaf
Ingo Breuer <inbre001>
2014-10-02 07:09:19 +0200

    -

---
+++ Example/src/subdir.mk
@@ -0,0 +1,60 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/Connection.cpp \
+../src/Example.cpp \
+../src/ServerSocket.cpp \
+../src/SocketFd.cpp \
+../src/TCPClientSocket.cpp \
+../src/TCPServerSocket.cpp \
+../src/TCPSocket.cpp \
+../src/Transceiver.cpp \
+../src/UDPClientSocket.cpp \
+../src/UDPServerSocket.cpp \
+../src/UDPSocket.cpp \
+../src/UNIXClientSocket.cpp \
+../src/UNIXServerSocket.cpp 
+
+OBJS += \
+./src/Connection.o \
+./src/Example.o \
+./src/ServerSocket.o \
+./src/SocketFd.o \
+./src/TCPClientSocket.o \
+./src/TCPServerSocket.o \
+./src/TCPSocket.o \
+./src/Transceiver.o \
+./src/UDPClientSocket.o \
+./src/UDPServerSocket.o \
+./src/UDPSocket.o \
+./src/UNIXClientSocket.o \
+./src/UNIXServerSocket.o 
+
+CPP_DEPS += \
+./src/Connection.d \
+./src/Example.d \
+./src/ServerSocket.d \
+./src/SocketFd.d \
+./src/TCPClientSocket.d \
+./src/TCPServerSocket.d \
+./src/TCPSocket.d \
+./src/Transceiver.d \
+./src/UDPClientSocket.d \
+./src/UDPServerSocket.d \
+./src/UDPSocket.d \
+./src/UNIXClientSocket.d \
+./src/UNIXServerSocket.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I../include -O0 -g -Wall -Wextra -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
